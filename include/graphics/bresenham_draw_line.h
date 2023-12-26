f7f1e3ec89580f527cd84627d7706aaf92e03d83
Blurring Shadow <976999484@qq.com>
2023-12-26 12:00:36 +0800

    refactor(bresenham): reimplementing bresenham_draw_line

--- include/graphics/bresenham_draw_line.h
+++ include/graphics/bresenham_draw_line.h
@@ -14,12 +14,21 @@ namespace graphics
         glm::vec<2, T, Qualifier> p0,
         const decltype(p0) p1,
         std::output_iterator<decltype(p1)> auto out_it,
-        std::make_unsigned_t<T>& delta_x = 1,
-        std::make_unsigned_t<T>& delta_y = 1,
+        const std::make_unsigned_t<T> delta_x = 1,
+        const std::make_unsigned_t<T> delta_y = 1
     )
     {
         const auto line_a = p0.y - p1.y;
         const auto line_b = p1.x - p0.x;
 
+        *(out_it++) = p0;
+
+        if((line_a > 0) == (line_b > 0))
+        {
+
+            return;
+        }
+
+        // a * b < 0 , if line_a * (x + dx) + line_b * (y + dy/2) > 0
     }
 }
