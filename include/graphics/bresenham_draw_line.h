d873ed3eb7d2a6890a826ec2326bce9b8d50aca6
Blurring Shadow <976999484@qq.com>
2024-01-05 17:31:29 +0800

    refactor(bresenham): reimplemented bresenham_draw_line

--- include/graphics/bresenham_draw_line.h
+++ include/graphics/bresenham_draw_line.h
@@ -12,55 +12,10 @@ namespace graphics
     inline constexpr struct bresenham_draw_line_fn
     {
     private:
-        template<typename T, glm::qualifier Qualifier>
-        static constexpr auto impl(
-            glm::vec<2, T, Qualifier> p0,
-            const T line_a,
-            const T line_b,
-            auto out_it,
-            T dx,
-            T dy
-        )
+        template<typename T>
+        static constexpr bool is_followed(const T l, const T r)
         {
-            const auto line_c = line_a * p0.x + line_b * p0.y;
-            const auto c_dy = line_b * dy;
-            const auto c_dx = line_a * dx;
-
-            auto x_diff = line_b;
-            auto y_diff = -line_a;
-
-            const auto c_dy_cmp = c_dy <=> 0;
-            const auto half_c_dy = c_dy / 2;
-            const auto two_line_c = 2 * line_c;
-
-            // a * (x + dx) + b * y
-            for(auto c = line_c; std::abs(x_diff) > std::abs(dx);
-                x_diff -= dx, p0.x += dx, c += c_dx, ++out_it)
-            {
-                if(y_diff != 0)
-                {
-                    // current_c = a * (x + dx * i) + b * (y + dy * j)
-                    // b * dy * k = current_c - c
-                    if(const auto step = (c - line_c + half_c_dy) / c_dy; step != 0)
-                    {
-                        auto d = dy * step;
-                        if(std::abs(y_diff) <= std::abs(d)) d = y_diff;
-
-                        p0.y += d;
-                        y_diff -= d;
-                        c += d * line_b;
-                    }
-                }
-
-                *out_it = p0;
-            }
-
-            if(std::abs(x_diff) > 0)
-            {
-                p0.x += x_diff;
-                p0.y += y_diff;
-                *out_it = p0;
-            }
+            return is_eq(l) || (l == r);
         }
 
     public:
@@ -73,38 +28,55 @@ namespace graphics
             T dy = 0
         ) const
         {
-            const auto line_a = p0.y - p1.y;
-            const auto line_b = p1.x - p0.x;
+            const auto diff = p1 - p0;
+            const auto x_diff_cmp = diff.x <=> 0;
+            const auto y_diff_cmp = diff.y <=> 0;
 
-            Expects((-dy <=> 0) == (line_a <=> 0));
-            Expects((dx <=> 0) == (line_b <=> 0));
+            Expects(is_followed(dx <=> 0, x_diff_cmp));
+            Expects(is_followed(dy <=> 0, y_diff_cmp));
 
-            if(dy == 0) dy = line_a < 0 ? 1 : -1;
+            if(dx == 0) dx = diff.x > 0 ? 1 : -1;
+            if(dy == 0) dy = diff.y > 0 ? 1 : -1;
 
-            if(line_b == 0)
+            if(diff.x == 0)
             {
-                if(line_a < 0)
-                    for(; p0.y <= p1.y; p0.y += dy, ++out_it) *out_it = p0;
-                else if(line_a > 0)
-                    for(; p0.y >= p1.y; p0.y += dy, ++out_it) *out_it = p0;
-                else *out_it = p0;
-
+                for(; is_followed(p1.y <=> p0.y, y_diff_cmp); p0.y += dy, ++out_it) *out_it = p0;
                 return;
             }
 
-            if(dx == 0) dx = line_b > 0 ? 1 : -1;
-
-            if(line_a == 0)
+            if(diff.y == 0)
             {
-                if(line_b > 0)
-                    for(; p0.x <= p1.x; p0.x += dx, ++out_it) *out_it = p0;
-                else if(line_b < 0)
-                    for(; p0.x >= p1.x; p0.x += dx, ++out_it) *out_it = p0;
-
+                for(; is_followed(p1.x <=> p0.x, x_diff_cmp); p0.x += dx, ++out_it) *out_it = p0;
                 return;
             }
 
-            impl(p0, line_a, line_b, out_it, dx, dy);
+            // a = diff.y;
+            // b = -diff.x;
+            // delta_c = a * dx + b * dy
+            const auto c_dx = diff.y * dx;
+            const auto c_dy = -diff.x * dy;
+            const auto half_c_dy = c_dy / 2;
+
+            // a * (x + dx) + b * y
+            for(T c = 0; is_followed(p1.x <=> p0.x, x_diff_cmp); p0.x += dx, c += c_dx)
+            {
+                // current_c = a * (x + dx * i) + b * (y + dy * j)
+                // c - current_c = b * dy * k
+                const auto c_cmp = c <=> 0;
+                if(const auto next_c = c + c_dy;is_followed(next_c <=> 0, c_cmp))
+                {
+                    for(; is_followed(next_c <=> 0, c_cmp); next_c += c_dy)
+                    {
+                        *(out_it++) = p0;
+                        p0.y += dy;
+
+                        if(!is_followed(p1.y <=> p0.y, y_diff_cmp)) return;
+                    }
+
+                    c = next_c;
+                }
+                else *(out_it++) = p0;
+            }
         }
     } bresenham_draw_line{};
 }
--- tests/src/bresenham_draw_line_test.cpp
+++ tests/src/bresenham_draw_line_test.cpp
@@ -6,7 +6,40 @@ using namespace stdsharp;
 using namespace glm;
 using namespace graphics;
 
-SCENARIO("draw test", "[bresenham_draw_line]")
+bool is_followed(const strong_ordering l, const strong_ordering r)
+{
+    return is_eq(l) || (l == r);
+}
+
+auto validate_vec(ivec2 p0, decltype(p0) p1, decltype(p0)::value_type dx)
+{
+    const auto x_diff = p1.x - p0.x;
+    const auto y_diff = p1.y - p0.y;
+    const auto x_diff_cmp = x_diff <=> 0;
+    const auto y_diff_cmp = y_diff <=> 0;
+
+    const auto line_a = y_diff;
+    const auto line_b = -x_diff;
+
+    vector<decltype(p0)> out;
+
+    if(x_diff == 0)
+    {
+        for(; is_followed(p1.y <=> p0.y, y_diff_cmp); p0.y += dx)
+            out.emplace_back(p0);
+         return ;
+    }
+
+    const auto line_c = -p0.x * line_a - p0.y * line_b;
+    size_t i = 0;
+    const auto x = p0.x + dx * i;
+    const auto y = static_cast<long double>(line_a * x + line_c) / -line_b;
+    const auto y_step = std::round((y - p0.y) / dx);
+
+    return {x, p0.y + dx * y_step};
+}
+
+SCENARIO("draw line", "[bresenham_draw_line]")
 {
     struct arg
     {
@@ -15,30 +48,26 @@ SCENARIO("draw test", "[bresenham_draw_line]")
         decltype(p0)::value_type dx;
 
         vector<decltype(p0)> out;
+
+        validate_iterator validator() const { return {{}, p0, p1, dx}; }
     };
 
     const auto args = GENERATE(
-        arg{
-            {0, 0},
-            {3, 3},
-            1,
-            {{0, 0}, {1, 1}, {2, 2}, {3, 3}} //
-        },
+        arg{{0, 0}, {3, 3}, 0},
         arg{
             {0, 0},
             {5, 2},
-            1,
+            0,
             {{0, 0}, {1, 0}, {2, 1}, {3, 1}, {4, 2}, {5, 2}} //
         },
         arg{
             {0, 0},
             {2, 5},
-            1,
-            {{0, 0}, {1, 3}, {2, 5}} //
+            0,
+            {{0, 0}, {1, 1}, {1, 2}, {1, 3}, {2, 4}, {2, 5}} //
         }
     );
 
-    decltype(args.out) out{args.out.size()};
 
     bresenham_draw_line(args.p0, args.p1, out.begin(), args.dx, args.dx);
 
