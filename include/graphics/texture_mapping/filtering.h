f309862ecfe252e252c28af5f1246c06ca5f7160
Blurring Shadow <976999484@qq.com>
2024-02-06 17:19:29 +0800

    feat(texture-filtering): implementing

--- include/graphics/texture_mapping/filtering.h
+++ include/graphics/texture_mapping/filtering.h
@@ -1,91 +1,135 @@
 #pragma once
 
 #include <range/v3/range/operations.hpp>
-#include <ranges>
-#include <stdsharp/concepts/concepts.h>
+#include <stdsharp/ranges/ranges.h>
 
-#include "../namespace_alias.h"
+#include "../traits.h"
+
+namespace stdsharp
+{
+#define STDSHARP_MD_RANGE_TRAITS(name, ns)                             \
+    template<std::size_t N, typename T>                                \
+    struct md_##name                                                   \
+    {                                                                  \
+        using rng = T;                                                 \
+                                                                       \
+        using type = ns::name##_t<typename md_##name<N - 1, T>::type>; \
+    };                                                                 \
+                                                                       \
+    template<typename T>                                               \
+    struct md_##name<0, T>                                             \
+    {                                                                  \
+        using rng = T;                                                 \
+                                                                       \
+        using type = ns::name##_t<T>;                                  \
+    };                                                                 \
+                                                                       \
+    template<std::size_t N, typename T>                                \
+    using md_##name##_t = typename md_##name<N, T>::type;
+
+    STDSHARP_MD_RANGE_TRAITS(iterator, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(const_iterator, stdsharp)
+    STDSHARP_MD_RANGE_TRAITS(sentinel, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(const_sentinel, stdsharp)
+    STDSHARP_MD_RANGE_TRAITS(range_size, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(range_difference, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(range_value, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(range_reference, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(range_const_reference, stdsharp)
+    STDSHARP_MD_RANGE_TRAITS(range_rvalue_reference, std::ranges)
+    STDSHARP_MD_RANGE_TRAITS(range_common_reference, std::ranges)
+
+#undef STDSHARP_MD_RANGE_TRAITS
+}
 
 namespace graphics::texture_mapping
 {
     inline constexpr struct bilinear_filtering_fn
     {
         template<
-            typename T,
-            glm::qualifier Q = glm::defaultp,
-            std::ranges::random_access_range TextureRng>
-            requires requires(std::ranges::range_reference_t<TextureRng> texture_rng) {
-                requires std::numeric_limits<T>::is_iec559;
-                requires std::ranges::random_access_range<decltype(texture_rng)>;
-                requires star::arithmetic_like<
-                    std::ranges::range_reference_t<decltype(texture_rng)>>;
-                requires star::explicitly_convertible<
-                    T,
-                    std::ranges::range_difference_t<decltype(texture_rng)>>;
+            std::ranges::random_access_range TextureRng,
+            star::explicitly_convertible<std::ranges::range_difference_t<TextureRng>> T = float,
+            glm::qualifier Q = glm::defaultp>
+            requires requires {
+                requires std::floating_point<T>;
+                requires std::ranges::random_access_range<
+                    std::ranges::range_reference_t<TextureRng>>;
+                requires star::
+                    explicitly_convertible<T, star::md_range_difference_t<0, TextureRng>>;
+                glm_vec_traits<star::md_range_reference_t<1, T>>{};
             }
         constexpr auto operator()(const glm::vec<2, T, Q>& uv, TextureRng&& rng) const
         {
+            using ::ranges::index;
+            using star::auto_cast;
+
             const auto& uv_floor = glm::floor(uv);
             const auto& uv_next = uv_floor + T{1};
             const auto& a = uv_next - uv;
             const auto& b = uv - uv_floor;
 
-            ::ranges::index;
+            using u_diff_t = std::ranges::range_difference_t<decltype(rng)>;
 
-            using diff_t = std::ranges::range_difference_t<decltype(rng)>;
+            const auto& u_floor = index(rng, static_cast<u_diff_t>(uv_floor.x));
+            const auto& u_next = index(rng, static_cast<u_diff_t>(uv_next.x));
 
-            const auto& u_floor_iter =
-                std::ranges::cbegin(std::ranges::cbegin(rng)[static_cast<diff_t>(uv_floor.x)]);
-            const auto& u_next_iter =
-                std::ranges::cbegin(std::ranges::cbegin(rng)[static_cast<diff_t>(uv_next.x)]);
+            using v_diff_t = star::md_range_difference_t<0, TextureRng>;
 
-            using iter_diff_t = std::iter_difference_t<std::decay_t<decltype(u_floor_iter)>>;
+            const v_diff_t v_floor = auto_cast(uv_floor.y);
+            const v_diff_t v_next = auto_cast(uv_next.y);
 
-            const auto& v_floor = static_cast<iter_diff_t>(uv_floor.y);
-            const auto& v_next = static_cast<iter_diff_t>(uv_next.y);
-
-            return a.y * (a.x * u_floor_iter[v_floor] + b.x * u_next_iter[v_floor]) +
-                b.y * (a.x * u_floor_iter[v_next] + b.x * u_next_iter[v_next]);
+            return a.y * (a.x * index(u_floor, v_floor) + b.x * index(u_next, v_floor)) +
+                b.y * (a.x * index(u_floor, v_next) + b.x * index(u_next, v_next));
         }
     } bilinear_filtering{};
 
     inline constexpr struct trilinear_filtering_fn
     {
         template<
-            typename T,
-            glm::qualifier Q = glm::defaultp,
-            std::ranges::random_access_range MinmapRng>
-            requires requires(std::ranges::range_reference_t<MinmapRng> texture_rng) {
-                requires std::numeric_limits<T>::is_iec559;
-                requires std::ranges::random_access_range<decltype(texture_urng)>;
-                requires star::arithmetic_like<
-                    std::ranges::range_reference_t<decltype(texture_urng)>>;
-                requires star::explicitly_convertible<
-                    T,
-                    std::ranges::range_difference_t<decltype(texture_urng)>>;
+            std::ranges::random_access_range MinmapRng,
+            star::explicitly_convertible<std::ranges::range_difference_t<MinmapRng>> U = float>
+        constexpr auto operator()(const auto& uv, const U& minmap_level, MinmapRng&& minmaps) const
+            requires requires {
+                requires std::floating_point<U>;
+                requires std::invocable<
+                    bilinear_filtering_fn,
+                    decltype(uv),
+                    std::ranges::range_reference_t<MinmapRng>>;
             }
-        constexpr auto operator()(
-            const glm::vec<2, T, Q>& uv,
-            const std::type_identity_t<T>& minmap_level,
-            MinmapRng&& minmaps,
-            std::input_iterator auto minmaps_interpolate_it
-        ) const
         {
+            using ::ranges::index;
+            using diff_t = std::ranges::range_difference_t<MinmapRng>;
+
+            const auto& minmap_level_floor = std::floor(minmap_level);
+            const auto& minmap_level_next = minmap_level_floor + 1;
+
+            return (minmap_level_next - minmap_level) *
+                bilinear_filtering(uv, index(minmaps, static_cast<diff_t>(minmap_level_floor))) +
+                (minmap_level - minmap_level_floor) *
+                bilinear_filtering(uv, index(minmaps, static_cast<diff_t>(minmap_level_next)));
         }
     } trilinear_filtering{};
 
     inline constexpr struct nearest_filtering_fn
     {
         template<
-            typename T,
-            glm::qualifier Q = glm::defaultp,
-            std::invocable<const glm::vec<2, T, Q>&> GetTextureFn>
-            requires star::arithmetic_like<
-                         std::invoke_result_t<GetTextureFn, const glm::vec<2, T, Q>&>> &&
-            std::numeric_limits<T>::is_iec559
-        constexpr auto operator()(const glm::vec<2, T, Q>& uv, GetTextureFn fn) const
+            std::ranges::random_access_range TextureRng,
+            star::explicitly_convertible<std::ranges::range_difference_t<TextureRng>> T = float,
+            glm::qualifier Q = glm::defaultp>
+            requires requires {
+                requires std::floating_point<T>;
+                requires std::ranges::random_access_range<
+                    std::ranges::range_reference_t<TextureRng>>;
+                requires star::
+                    explicitly_convertible<T, star::md_range_difference_t<1, TextureRng>>;
+            }
+        constexpr auto operator()(glm::vec<2, T, Q> uv, TextureRng&& rng) const
         {
-            return std::invoke(fn, glm::round(uv));
+            using ::ranges::index;
+
+            uv = glm::round(uv);
+
+            return index(index(rng, static_cast<std::ranges::range_difference_t<TextureRng>>(uv.x)), static_cast<star::md_range_difference_t<1, TextureRng>>(uv.y));
         }
     } nearest_filtering{};
 }
